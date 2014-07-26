703eb90588d892017ab05a0868533e84f7ef230e
anprogrammer <churcd@alum.rpi.edu>
2014-07-25 21:33:19 -0400

    Includes missing DLL

--- OpenRoads/update_node_deps.bat
+++ OpenRoads/update_node_deps.bat
@@ -2,6 +2,7 @@ PUSHD
 cd Node
 cmd /C "npm install F:\Documents\GitHub\node-webgl-ovr"
 cd node_modules\node-webgl-ovr
+copy F:\Libs\FreeImage\Release\*.dll build\Release
 cmd /C "npm install F:\Documents\GitHub\node-glfw-ovr"
 cd node_modules\node-glfw-ovr\build\Release\
 copy F:\Libs\AntTweakBar\lib\AntTweakBar.dll .
